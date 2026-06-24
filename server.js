1f3c5838b8392323f970bafb9498ad358a888e4a
abuazmalmagedi-cmd <abuazmalmaagedi@gmail.com>
2026-06-24 11:23:15 +0300

    Fix syntax and host to 0.0.0.0

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OBSD Bot is Running...\n');
-}).listen(PORT, () => {
+}).listen(PORT,'0.0.0.0' () => {
     console.log(`📡 Web server is listening on port ${PORT}`);
 });
